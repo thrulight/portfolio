aed1207f82b4e8fd37d9539e0773e084d4fd9376
thrulight <e.durand.04@gmail.com>
2022-10-26 17:01:44 +0200

    -- slide in animation

--- script.js
+++ script.js
@@ -150,5 +150,21 @@ if ( $(window).width() <= 900 ) {
     }
     prevScrollpos = currentScrollPos;
   }
-
 }
+
+// slide in animation
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
