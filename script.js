1ef31c9cfdd1d8dc7739debcf57fe8dbb675c615
thrulight <e.durand.04@gmail.com>
2022-10-26 17:17:20 +0200

    -- animations on About page

--- script.js
+++ script.js
@@ -168,3 +168,19 @@ $(document).on("scroll", function() {
     }
   }
 });
+
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag2");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
