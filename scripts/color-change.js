d65b79a2da71369456756288c856cec6dbb8d645
thrulight <e.durand.04@gmail.com>
2023-05-20 15:54:57 +0200

    -- UX/UI in first

--- scripts/color-change.js
+++ scripts/color-change.js
@@ -6,7 +6,7 @@ $( window ).ready(function() {
     $('.slide')
       .height(clientHeight)
       .scrollie({
-        scrollOffset : -50,
+        scrollOffset : -500, //Offset for color change. Higher requires more offset
         scrollingInView : function(elem) {
 
           var bgColor = elem.data('background');
