1cc3451a41161462cc931b84ca01702285de2dd8
thrulight <e.durand.04@gmail.com>
2023-08-30 19:23:18 +0200

    -- oriane projects

---
+++ scripts/digital/oriane-translation.js
@@ -0,0 +1,193 @@
+var LanguageList = {
+  "EN" : "EN",
+  "FR" : "FR"
+};
+
+//languages Objects
+var WORDS_EN = {
+
+//Nav bar
+"shop" : "shop",
+"about" : "about",
+"work" : "work",
+
+//Description
+"header" : "Designing a physical solution on the subject of high-school dropouts, by involving the parents a little bit more in the school-life.",
+"year" : "year",
+"duration" : "duration",
+"durationText" : "4 months",
+"role" : "my role",
+"roleText" : "General interest designer",
+"team" : "team",
+"client" : "Ile-de-France Regional Council, Oriane Agency",
+
+//Project
+"project" : "The different parts of the project",
+
+//Kiosque
+"kiosqueTitle" : "The Kiosk 182",
+"kiosqueText" : "Giving access to information concerning the highschool",
+"kiosqueParagraph" : "Parents have a central role in educating their children, and having access to all the proper information to guide them properly is necessary.",
+
+//Initial goal
+"initialGoalTitle" : "The initial goal",
+"initialGoalText" : "Designing a solution for parents whose children are in high-school, to give them more access to information concerning its education",
+"initialGoalParagraph" : "The parents are given information papers throughout the year, to learn on many different subjects. These pieces of paper are separated in four categories.",
+"initialGoalStep1" : "General information",
+"initialGoalStep2" : "Stakeholders",
+"initialGoalStep3" : "Thematic workshops",
+"initialGoalStep4" : "Parent-teacher meetings",
+
+//Calendar
+"calendarTitle" : "Calendar",
+"calendarText" : "A main part of this project was designing a calendar to better understand the key moments of a school year",
+"calendarParagraph1" : "The information that can be found on it are school events, such as parent-teacher meetings, the parents’ back-to-school, class councils, vacations.",
+"calendarParagraph2" : "There are also events happening outside of the school, such as career fairs, and understand when students are the most subject to dropping out of school, and what are the objectives for each key moment.",
+"calendarCaption" : "A place on the bottom was added to let parents write down what they need to, to get to grips with the calendar.",
+
+//User Scenario
+"scenarioText" : "The user scenario",
+"scenarioCaption" : "The calendar and information sheets are given in the shape of a binder that can open and be hung on the wall, putting the calendar above, and the binder below.",
+
+//Methology
+"methodology" : "Methodology",
+
+"interviewing" : "Interviewing",
+"interviewingParagraph" : "The subject being quite complex, we had to interview lots of stakeholders to get a grasp of how the whole ecosystem functions, as well as interviewing final users of our project.",
+
+"ideating" : "Ideating",
+"ideatingParagraph1" : "As we progressed in the project, we had to brainstorm to define the different parts of it.",
+"ideatingParagraph2" : "A lot of elements needed to be defined, as we worked on the project. We used Miro to help us organize our ideas and define our ideas.",
+
+"researching" : "Researching",
+"researchingParagraph" : "To stay organized in our researches, documents, ideas and interviews, we created a space in Notion. It helped us also track our tasks to make sure everything was on track.",
+
+//Navigation Bottom
+"back" : "back to projects",
+"next" : "next project",
+
+//Footer
+"getInTouch" : "Get in touch",
+"coded" : "This website is designed and coded by",
+"me" : "me",
+"license" : "All projects are licensed under"
+
+};
+
+var WORDS_FR = {
+
+//Nav bar
+"shop" : "boutique",
+"about" : "a propos",
+"work" : "projects",
+
+//Description
+"header" : "Concevoir une solution physique au problème du décrochage scolaire, en impliquant davantage les parents dans la vie de l'école.",
+"year" : "année",
+"duration" : "durée",
+"durationText" : "4 mois",
+"role" : "mon rôle",
+"roleText" : "Designer d'intérêt général",
+"team" : "équipe",
+"client" : "Conseil Régional d'Ile-de-France, Agence Oriane",
+
+//Project
+"project" : "Les différentes parties du projet",
+
+//Kiosque
+"kiosqueTitle" : "Le Kiosque 182",
+"kiosqueText" : "Donner accès à plus d'informations concernant le lycée",
+"kiosqueParagraph" : "Les parents ont un rôle central dans l'éducation de leurs enfants, et il est nécessaire qu'ils aient accès à toutes les informations nécessaires afin de les guider correctement.",
+
+//Initial goal
+"initialGoalTitle" : "L'objectif initial",
+"initialGoalText" : "Concevoir une solution pour les parents dont les enfants sont au lycée, afin de leur donner un meilleur accès à l'information concernant son éducation",
+"initialGoalParagraph" : "Tout au long de l'année, les parents reçoivent des fiches d'information sur de nombreux sujets. Ces documents sont classés en quatre catégories.",
+"initialGoalStep1" : "Information générale",
+"initialGoalStep2" : "Acteurs",
+"initialGoalStep3" : "Ateliers thématiques",
+"initialGoalStep4" : "Réunions parents-profs",
+
+//Calendar
+"calendarTitle" : "Calendrier",
+"calendarText" : "Une partie importante de ce projet consistait à concevoir un calendrier pour mieux comprendre les moments clés d'une année scolaire",
+"calendarParagraph1" : "Les informations que l'on peut y trouver sont des événements scolaires, tels que les réunions parents-professeurs, la rentrée scolaire des parents, les conseils de classe et les vacances.",
+"calendarParagraph2" : "Il y a également des événements qui se déroulent en dehors de l'école, tels que des salons de l'orientation, et il faut comprendre à quel moment les élèves sont le plus susceptibles de décrocher, et quels sont les objectifs pour chaque moment clé.",
+"calendarCaption" : "Un espace en bas de page a été ajouté pour permettre aux parents d'écrire ce dont ils ont besoin pour s'approprier le calendrier.",
+
+//User Scenario
+"scenarioText" : "Le scénario d'usage",
+"scenarioCaption" : "Le calendrier et les fiches d'information sont présentés sous la forme d'un classeur qui peut s'ouvrir et être accroché au mur, en plaçant le calendrier au-dessus et le classeur en dessous.",
+
+//Methology
+"methodology" : "Méthodologie",
+
+"interviewing" : "Interviewer",
+"interviewingParagraph" : "Le sujet étant assez complexe, nous avons dû interroger de nombreuses parties prenantes pour comprendre le fonctionnement de l'ensemble de l'écosystème, ainsi que les utilisateurs finaux de notre projet.",
+
+"ideating" : "Idéation",
+"ideatingParagraph1" : "Au fur et à mesure que nous avancions dans le projet, nous avons dû faire un brainstorming pour en définir les différentes parties.",
+"ideatingParagraph2" : "De nombreux éléments devaient être définis au fur et à mesure que nous travaillions sur le projet. Nous avons utilisé Miro pour nous aider à organiser nos idées.",
+
+"researching" : "Recherche",
+"researchingParagraph" : "Pour rester organisés dans nos recherches, nos documents, nos idées et nos entretiens, nous avons créé un espace dans Notion. Cela nous a également permis de suivre nos tâches afin de nous assurer que tout se déroulait comme prévu.",
+
+//Navigation Bottom
+"back" : "retourner aux projets",
+"next" : "prochains projets",
+
+//Footer
+"getInTouch" : "Me contacter",
+"coded" : "Ce site a été conçu et codé par",
+"me" : "moi",
+"license" : "Tous les projets sont soumis aux licenses"
+
+};
+
+window.onload = initialize;
+
+function initialize() {
+  var $dropdown = $("#country_select");
+  $.each(LanguageList, function(key, value) {
+    $dropdown.
+    append($("<option/>").
+    val(key).
+    text(value));
+  });
+
+  loadsLanguage("EN");
+}
+
+function loadsLanguage(lang){
+/*fills all the span tags with class=lang pattern*/
+  $('span[class^="lang"]').each(function(){
+    var LangVar = (this.className).replace('lang-','');
+    var Text = window["WORDS_"+lang][LangVar];
+    $(this).text(Text);
+  });
+
+  if (lang === 'EN') {
+    const frenchElements = document.getElementsByClassName('french');
+    for (let i = 0; i < frenchElements.length; i++) {
+      frenchElements[i].style.display = 'none';
+    }
+    const englishElements = document.getElementsByClassName('english');
+    for (let i = 0; i < englishElements.length; i++) {
+      englishElements[i].style.display = 'block';
+    }
+    //change HTML language in English
+    document.documentElement.setAttribute('lang','en');
+
+  } else if (lang === 'FR') {
+    const frenchElements = document.getElementsByClassName('french');
+    for (let i = 0; i < frenchElements.length; i++) {
+      frenchElements[i].style.display = 'block';
+    }
+    const englishElements = document.getElementsByClassName('english');
+    for (let i = 0; i < englishElements.length; i++) {
+      englishElements[i].style.display = 'none';
+    }
+    //change HTML language in French
+    document.documentElement.setAttribute('lang','fr');
+  }
+}
